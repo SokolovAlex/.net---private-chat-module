1f34f32ff8449dbf98d8528a330e17aa07e12687
Alexey Sokolov <alex.sklv85@gmail.com>
2015-09-29 17:23:46 +0600

    some done files to reilize chat

---
+++ Dal/Repositories/User2UserChat_UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.BLL.Common.Models.User;
+using App.BLL.Common.Models.User2UserChat;
+using App.DAL.DataBaseModel;
+using EmitMapper;
+
+namespace App.DAL.Concrete.Repos
+{
+    public partial class UserRepository
+    {
+        public IEnumerable<UserModel> GetAllRecipientsFor(int id)
+        {
+            var dbUsers = _context.USER.Where(x => x.ID != id).ToList();
+            return dbUsers.Select(x => ObjectMapperManager.DefaultInstance.GetMapper<USER, UserModel>().Map(x));
+        }
+
+        public PrivateChatUsers GetChatUsersByHashes(string senderHash, string recipientHash)
+        {
+            var users = _context.USER.Where(x => x.HashID == senderHash || x.HashID == recipientHash).ToList();
+            var sender = users.FirstOrDefault(x => x.HashID == senderHash);
+            var recipient = users.FirstOrDefault(x => x.HashID == recipientHash);
+
+            var result = new PrivateChatUsers();
+
+            if (sender == null || recipient == null)
+            {
+                return result;
+            }
+
+            result.Sender = ObjectMapperManager.DefaultInstance.GetMapper<USER, UserModel>().Map(sender);
+            result.Recipient = ObjectMapperManager.DefaultInstance.GetMapper<USER, UserModel>().Map(recipient);
+
+            return result;
+        }
+    }
+}
